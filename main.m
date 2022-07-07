42e40f38956fe8d56ed9c1ce79d9185f072c3826
unknown <moeinferdavani1380@gmail.com>
2022-07-07 17:54:33 +0430

    add energy map using a linear combination of the energy map of gradian and saliency and depth weighted with variance of otsu and then add k factor to deleteSeam karving that prevent remove neighbors line from picture

--- main.m
+++ main.m
@@ -1,7 +1,14 @@
-clc;
+
 clc;
 close all;
-I = im2double(imread("Images\Pagodas_en_el_lago_Shanhu_guilin.jpg"));
+disp(f(0));
+disp(f(1/4));
+disp(f(1/2));
+disp(f(3/4));
+disp(f(7/8));
+disp(f(15/16));
+disp(f(1));
+I = im2double(imread("Images\Baby.png"));
 figure,imshow(I);
 R = I(:,:,1);
 G = I(:,:,2);
@@ -15,10 +22,27 @@ v_gradian = imfilter(J,vertical_sobel);
 gradian = sqrt((h_gradian).^2+(v_gradian).^2);
 %normalize gradian
 max_gradian = max(max(gradian));
+eg = gradian/max_gradian;
+dg = getVareOfOtsu(eg);
+es = im2double(imread("Images\Baby_SMap.png"));
+ds = getVareOfOtsu(es);
+ed = im2double(imread("Images\Baby_DMap.png"));
+dd = getVareOfOtsu(ed);
+d_sum = dg + ds + dd;
+dg = (d_sum - dg)^2;
+ds = (d_sum - ds)^2;
+dd = (d_sum - dd)^2;
+gradian = dg*eg + ds*es + dd*ed;
+max_gradian = max(max(gradian));
 gradian = gradian/max_gradian;
+%disp(strcat('d:',num2str(getAvgDiffrenceOfOtsu(gradian))));
+%h = hist(gradian);
+plot(h);
 figure,imshow(gradian);
 figure,imshow(gradian);
+
 for n=1:round(.5 * size(R,2))
+    tic
 seam = getVerticalSeam(J,gradian);
 
 K = I;
@@ -28,19 +52,21 @@ for i =1:size(J,1)
     K(i,j,2) = 0;
     K(i,j,3) = 0;
 end
-J = removeVerticalSeam(J,seam);
-gradian = removeVerticalSeam(gradian,seam);
+J = removeVerticalSeam(J,seam,1);
+gradian = removeVerticalSeam(gradian,seam,1.2);
+    
 I = removeVerticalSeamTreeChannel(I,seam);
-
+toc
 imshow(K);
 
-disp(round(n/size(I,2)*100));
+disp(round(n/size(R,2)*100));
 
 pause(.01);
 
 end
 
 figure,imshow(I);
+
 
 %functions
 function columns = getVerticalSeam(I,E)
@@ -100,12 +126,18 @@ end
 
 end
 
-function J = removeVerticalSeam(I,columns)
+function J = removeVerticalSeam(I,columns,k)
 J = zeros(size(I,1),size(I,2) - 1,'double');
 for i=1:size(I)
     %befor remove
     J(i,1:columns(i) - 1) = I(i,1:columns(i) - 1);
     J(i,columns(i):(size(I,2) - 1)) = I(i,(columns(i) + 1): size(I,2));
+    if(columns(i) > 1)
+        J(i,columns(i) - 1) = k * J(i,columns(i) - 1);
+    end
+    if(columns(i)<size(I,2))
+        J(i,columns(i)) = k * J(i,columns(i));
+    end
 end
 
 end
@@ -114,8 +146,33 @@ function J = removeVerticalSeamTreeChannel(I,columns)
 R = I(:,:,1);
 G = I(:,:,2);
 B = I(:,:,3);
-R = removeVerticalSeam(R,columns);
-G = removeVerticalSeam(G,columns);
-B = removeVerticalSeam(B,columns);
+R = removeVerticalSeam(R,columns,1);
+G = removeVerticalSeam(G,columns,1);
+B = removeVerticalSeam(B,columns,1);
 J = cat(3,R,G,B);
+end
+function J = f(I)
+J = I.*(((1/2)^(2/3))*((abs(I - 1/2))^(1/3))*(I>1/2) + 1/2);
+end
+function E = getSaliencyMap(I)
+end
+function d = getVareOfOtsu(I)
+level = multithresh(I);
+bw = im2bw(I,level);
+%figure,imshow(bw);
+%imshow(bw);
+K1 = [];
+K2 = [];
+for i=1:size(I,1)
+    for j=1:size(I,2)
+        if bw(i,j) == 1
+            K1(size(K1,2)+1) = I(i,j);
+        else
+            K2(size(K2,2)+1) = I(i,j);
+        end
+    end
+end
+v1 = var(K1(:));
+v2 = var(K2(:));
+d = v1*numel(K1) + v2*numel(K2);
 end
